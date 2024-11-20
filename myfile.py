eccc644f52f8d08b94cf0801370ea5884022f3cf
tetteyA <tetteyabigail6@gmail.com>
2024-11-20 11:17:51 +0000

    initial comment

---
+++ myfile.py
@@ -0,0 +1,33 @@
+# x=5
+# y="John"
+# print(type(x))
+# print(type(y))
+# a,b,c = "cat","dog","mouse"
+# print(a,b,c)
+# x=y=z="orange"
+# print(x,y,z)
+# fruits= ['apple','coconut','banana']
+# x,y,z=fruits
+# print(x)
+# print(y)
+# print(z)
+# x= "Abigail "
+# y= "is "
+# z= "pretty"
+# print(x+y+z)
+
+
+# def f():
+#     s= "Abigail"
+#     print ("Inside Function",s)
+
+# s= "I love Geeksforgeeks"
+# f()
+# print("Outside Function",s)
+
+# x= "splendid"
+
+def myfunction(print):
+  x="awesone"
+
+myfunction()
